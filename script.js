23e53edc044568566ef84ce27f1df0301e1164e3
Ipsita Nag <titli.nag04@gmail.com>
2025-10-19 02:52:36 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-//dropdown toggle function
-
-function toggleDropdown(event) {
-  event.preventDefault(); // prevents link navigation
-  const dropdown = event.target.closest('.dropdown');
-  dropdown.classList.toggle('active');
-}
